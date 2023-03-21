d76ed793ce94d7daf1cd9e21035898f257315562
Yahyamoftah <yahyatech2018@gmail.com>
2023-03-21 13:42:28 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
+#include "main.h"
+
 /**
- * main - entry point
- *
- * Return: Always 0.
- */
+* main - is the start
+* Return: 0 every were
+*/
+
 int main(void)
 {
-	char *str = "_putchar\n";
-
-	while (*str != '\0')
-		_putchar(*str++);
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
