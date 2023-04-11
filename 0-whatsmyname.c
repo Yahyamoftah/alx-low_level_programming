45dae2ea3d826bc6ce764cf7d213d5735ed9fc3d
Yahyamoftah <yahyatech2018@gmail.com>
2023-04-10 17:02:01 -0700

    update

---
+++ 0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - func prints name.
+ * @argc: the number.
+ * @argv: array contains program arguments.
+ * Return: returns 0 
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
