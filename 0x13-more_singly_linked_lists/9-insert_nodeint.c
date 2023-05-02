39a2fdae0c2fd052f064ecdd84685a4980326ff9
Yahyamoftah <yahyatech2018@gmail.com>
2023-05-02 14:50:44 -0700

    update

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - main finc
+ * @head: an inpppp
+ * @idx: an inpppp
+ * @n: an inppp
+ * Return: returns 0
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+listint_t *new_node, *koky;
+unsigned int i;
+if (head == NULL)
+return (NULL);
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+if (idx == 0)
+{
+new_node->next = *head;
+*head = new_node;
+return (new_node);
+}
+koky = *head;
+for (i = 0; i < idx - 1 && koky != NULL; i++)
+koky = koky->next;
+if (koky == NULL)
+{
+free(new_node);
+return (NULL);
+}
+new_node->next = koky->next;
+koky->next = new_node;
+return (new_node);
+}
