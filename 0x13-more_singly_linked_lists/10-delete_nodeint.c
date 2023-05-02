95933bbd1981ce2b2aa7d061fe800a6f41601f04
Yahyamoftah <yahyatech2018@gmail.com>
2023-05-02 14:55:49 -0700

    update

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - main func
+ * @head: an inpppp
+ * @index: an inppppp
+ * Return: returns 1
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+listint_t *current, *temp;
+unsigned int i;
+if (head == NULL || *head == NULL)
+return (-1);
+current = *head;
+if (index == 0)
+{
+*head = current->next;
+free(current);
+return (1);
+}
+for (i = 0; i < index - 1 && current != NULL; i++)current = current->next;
+if (current == NULL || current->next == NULL)
+return (-1);
+temp = current->next;
+current->next = temp->next;
+free(temp);
+return (1);
+}
