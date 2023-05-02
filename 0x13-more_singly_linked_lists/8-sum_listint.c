ffd6905c1eb38ae65ee37373c30c164254b28c84
Yahyamoftah <yahyatech2018@gmail.com>
2023-05-02 14:30:55 -0700

    update

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,16 @@
+/**
+ * sum_listint - main finc
+ * @head: an input
+ * Return: the sum
+ */
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+listint_t *node = head;
+while (node)
+{
+sum += node->n;
+node = node->next;
+}
+return (sum);
+}
