88896e4ad46ccf3bb60c23d871df8959931049fd
Yahyamoftah <yahyatech2018@gmail.com>
2023-05-08 13:46:58 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,17 +9,17 @@ ssize_t fd, bytes_read, bytes_written;
 char *buffer;
 
 if (filename == NULL)
-return 0;
+return (0);
 
 buffer = malloc(sizeof(char) * (letters + 1));
 if (buffer == NULL)
-return 0;
+return (0);
 
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 {
 free(buffer);
-return 0;
+return (0);
 }
 
 bytes_read = read(fd, buffer, letters);
@@ -27,20 +27,21 @@ if (bytes_read == -1)
 {
 free(buffer);
 close(fd);
-return 0;
+return (0);
 }
 
-buffer[bytes_read] = '\0';  
+buffer[bytes_read] = '\0';
 
 bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 if (bytes_written == -1 || bytes_written != bytes_read)
 {
 free(buffer);
 close(fd);
-return 0;
+return (0);
 }
 
 free(buffer);
 close(fd);
-return bytes_written;
+return (bytes_written);
 }
+
