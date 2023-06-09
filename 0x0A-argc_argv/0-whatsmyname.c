a1ceb01b92cbef76c02f7854c4fcdc2eec7b0a11
Yahyamoftah <yahyatech2018@gmail.com>
2023-06-09 10:04:20 -0700

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * @argc: num of arg
+ * @argv: arry of arg
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main func
+ * @argc: num of arg
+ * @argv: arry of arg
+ * Return: 0
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
