c7b9dbe540571e319972b179a2400ef3b2f10b30
Yahyamoftah <yahyatech2018@gmail.com>
2023-06-09 10:51:41 -0700

    update

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main
+ * @argc: num of arg
+ * @argv: arry of arg
+ * Return: 0 on success, 1 on error
+ */
+
+
+int main(int argc, char **argv)
+{
+int amount, numCoins = 0;
+int coins[] = {25, 10, 5, 2, 1};
+int numCoinTypes = sizeof(coins) / sizeof(coins[0]);
+
+if (argc != 2)
+{
+printf("Error\n");
+return 1;
+}
+
+amount = atoi(argv[1]);
+
+for (int i = 0; i < numCoinTypes; i++)
+{
+while (amount >= coins[i])
+{
+amount -= coins[i];
+numCoins++;
+}
+}
+
+printf("%d\n", numCoins);
+return 0;
+}
+
