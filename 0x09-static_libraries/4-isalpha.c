268a73598d3919a5251dda47b7123d624263c254
Yahyamoftah <yahyatech2018@gmail.com>
2023-06-09 08:41:25 -0700

    update

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha - check if the character is alphabet or not
+ * @c: checking the character
+ *
+ * Return: returns 1 if the character is alphabet and 0 if not
+ */
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* _putchar writes character c for  stdout
+* @c: character to print
+*
+* Return: On success 1
+* On error, -1 is returned
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
