d9cc629630da09baaed168d0962a422ce33bab36
Yahyamoftah <yahyatech2018@gmail.com>
2023-06-15 11:17:33 -0700

    update

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,16 +13,16 @@ int *arr;
 int size, i;
 
 if (min > max)
-return NULL;
+return (NULL);
 
 size = max - min + 1;
 arr = malloc(sizeof(int) * size);
 if (arr == NULL)
-return NULL;
+return (NULL);
 
 for (i = 0; i < size; i++)
 arr[i] = min++;
 
-return arr;
+return (arr);
 }
 
