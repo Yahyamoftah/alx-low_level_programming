8383bf3048700dc74ea454eaeeef1bd8eff1fb9b
Yahyamoftah <yahyatech2018@gmail.com>
2023-06-15 11:22:20 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,34 +11,34 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *ptr;
-    unsigned int len1, len2, concat_len, i, j;
+char *ptr;
+unsigned int len1, len2, concat_len, i, j;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-    len1 = strlen(s1);
-    len2 = strlen(s2);
+len1 = strlen(s1);
+len2 = strlen(s2);
 
-    if (n >= len2)
-        n = len2;
+if (n >= len2)
+n = len2;
 
-    concat_len = len1 + n;
+concat_len = len1 + n;
 
-    ptr = malloc(sizeof(char) * (concat_len + 1));
-    if (ptr == NULL)
-        return (NULL);
+ptr = malloc(sizeof(char) * (concat_len + 1));
+if (ptr == NULL)
+return (NULL);
 
-    for (i = 0; i < len1; i++)
-        ptr[i] = s1[i];
+for (i = 0; i < len1; i++)
+ptr[i] = s1[i];
 
-    for (j = 0; j < n; j++)
-        ptr[i++] = s2[j];
+for (j = 0; j < n; j++)
+ptr[i++] = s2[j];
 
-    ptr[i] = '\0';
+ptr[i] = '\0';
 
-    return (ptr);
+return (ptr);
 }
 
