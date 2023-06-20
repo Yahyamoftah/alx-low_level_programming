1b0b0533cd68eb014a728a91d74565a4a87ce83a
Yahyamoftah <yahyatech2018@gmail.com>
2023-06-20 15:19:21 -0700

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,11 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG
+#define DOG
 
 /**
  * struct dog - main
- * @name: input
- * @age: input
- * @owner: input
+ * @name: inp
+ * @age: inp
+ * @owner: inp
  */
 
 struct dog
@@ -17,7 +17,11 @@ char *owner;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
 
 #endif
